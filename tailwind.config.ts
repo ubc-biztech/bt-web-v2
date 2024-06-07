31526bd93acc88ac972e09ddc995c47117933536
m20arcusk <marcuskam20@gmail.com>
2024-06-07 11:37:05 -0700

    Added fonts and colors from the figma file into the tailwind.config.ts file. Also added import statement to global.css file for fonts.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,41 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary': {
+        'color': '#162039', // biztech navy
+      },
+      'secondary': {
+        'color': '#7AD040', // biztech green
+      },
+      // Login Page Colors
+      'login': {
+        'page-bg': '#11192E',
+        'form-card': '#1C253D',
+      },
+      // Events Page Colors
+      'events': {
+        'navigation-bg': '#11192E',
+        'active-tab-bg': '#1C253D',
+      },
+      // Selected Color Palette
+      'biztech-navy': '#162039',
+      'biztech-green': '#7AD040',
+      'dark-navy': '#0C1221',
+      'desat-navy': '#A2A7B3',
+      'baby-blue': '#B2C9FC',
+      'light-blue': '#E0E9FE',
+      'white-blue': '#F7FAFF',
+      'dark-green': '#55922D',
+      'light-green': '#AFE38C',
+      'white-green': '#F2FAEC',
+      'light-red': '#FF8686',
+    },
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
