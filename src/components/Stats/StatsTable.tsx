8de449c19095ab79c4765af1ad6821ebe1f8d33f
isa-leroux448 <isa.leroux448@gmail.com>
2024-07-02 00:14:40 -0700

    add title and data props to stats table

--- src/components/Stats/StatsTable.tsx
+++ src/components/Stats/StatsTable.tsx
@@ -1,4 +1,3 @@
-// sample data
 const dietaryRestrictions = {
   None: 200,
   Vegetarian: 20,
@@ -7,13 +6,21 @@ const dietaryRestrictions = {
   Pescetarian: 4,
 };
 
-const StatsTable = () => {
+interface StatsTableProps {
+  title?: string;
+  data?: Object;
+}
+
+const StatsTable: React.FC<StatsTableProps> = ({
+  title = "Dietary Restrictions",
+  data = dietaryRestrictions,
+}) => {
   return (
     <div className="bg-dark-slate p-8 w-1/3 rounded-md">
-      <h5 className="text-white">Dietary Restrictions</h5>
+      <h5 className="text-white">{title}</h5>
       <br />
       <div className="grid grid-cols-2 gap-2 text-pale-blue">
-        {Object.entries(dietaryRestrictions).map(
+        {Object.entries(data).map(
           ([restriction, count], index) => (
             <>
               <div className="col-span-1">{restriction}</div>
