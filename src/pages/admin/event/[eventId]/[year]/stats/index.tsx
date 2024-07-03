2f55c9e676da3e0b5d8a50f53e4cfdd61c032d2b
allant <allan.zf.tan@gmail.com>
2024-07-03 16:46:39 -0400

    put helper in utils

--- src/pages/admin/event/[eventId]/[year]/stats/index.tsx
+++ src/pages/admin/event/[eventId]/[year]/stats/index.tsx
@@ -1,13 +1,13 @@
-import { Attendee } from "@/components/RegistrationTable/columns";
 import { BasicInformation } from "@/types";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import { fetchRegistrationData } from "@/lib/dbUtils";
 
 type Props = {
     initialData: BasicInformation[] | null
 }
 
-export default function Statistics() {
+export default function Statistics( { initialData }: Props) {
     const router = useRouter()
 
     return (
@@ -31,33 +31,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const { eventId, year } = context.params as { eventId: string, year: string };
 
     try {
-        const data = await fetchRegistationData(eventId, year);
+        const data = await fetchRegistrationData(eventId, year);
         return { props: { initialData: data } };
     } catch (error) {
         console.error("Failed to fetch initial data:", error);
         return { props: { initialData: null } };
     }
-}
-
-async function fetchRegistationData(eventId: string, year: string) {
-    // TODO - fetch data registration data from backend. This is just returning a Mock, likely won't be the final data struct format
-
-    let data = []
-    for (let i = 0; i < 200; i++) {
-        data.push({
-            id: i.toString(),
-            regStatus: "Checked-In",
-            appStatus: "Accepted",
-            firstName: "John",
-            lastName: "Smith",
-            email: "testing@ubcbiztech.com",
-            points: 0,
-            studentNumber: "12345678",
-            faculty: "Comm...",
-            dynamicField1: "aa...",
-            shouldNotDisplay: "THIS SHOULD NOT BE DISPLAYING."
-        })
-    }
-
-    return data
 }
