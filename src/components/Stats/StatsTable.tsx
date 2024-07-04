2a2a3c29b157fd222c8d84f8b0d7e6c37bea2be0
isa-leroux448 <isa.leroux448@gmail.com>
2024-07-03 23:16:36 -0700

    add nested object compatibility

--- src/components/Stats/StatsTable.tsx
+++ src/components/Stats/StatsTable.tsx
@@ -1,33 +1,44 @@
-const dietaryRestrictions = {
-  None: 200,
-  Vegetarian: 20,
-  Vegan: 4,
-  Halal: 8,
-  Pescetarian: 4,
-};
-
 interface StatsTableProps {
-  title?: string;
-  data?: Object;
+  title: string;
+  data: Object;
 }
 
-const StatsTable: React.FC<StatsTableProps> = ({
-  title = "Dietary Restrictions",
-  data = dietaryRestrictions,
-}) => {
+const StatsTable: React.FC<StatsTableProps> = ({ title, data }) => {
+  const renderData = (data: Object, marginValue: number = 0): JSX.Element[] => {
+    return Object.entries(data).map(([restriction, count]) => {
+      if (typeof count === "number" || typeof count === "string") {
+        return (
+          <>
+            <div
+              className="col-span-2"
+              style={{ marginLeft: `${marginValue}px` }}
+            >
+              {restriction}
+            </div>
+            <div className="col-span-1 text-right">{count}</div>
+          </>
+        );
+      } else {
+        return (
+          <>
+            <div
+              className="col-span-2"
+              style={{ marginLeft: `${marginValue}px` }}
+            >
+              {restriction}
+            </div>
+            {renderData(count, marginValue + 16)}
+          </>
+        );
+      }
+    });
+  };
   return (
     <div className="bg-dark-slate p-8 w-1/3 rounded-md">
       <h5 className="text-white">{title}</h5>
       <br />
-      <div className="grid grid-cols-2 gap-2 text-pale-blue">
-        {Object.entries(data).map(
-          ([restriction, count], index) => (
-            <>
-              <div className="col-span-1">{restriction}</div>
-              <div className="col-span-1 text-right">{count}</div>
-            </>
-          )
-        )}
+      <div className="grid grid-cols-3 gap-2 text-pale-blue">
+        {renderData(data)}
       </div>
     </div>
   );
