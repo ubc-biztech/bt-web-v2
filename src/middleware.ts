b3c59f58041123d3f0a4196c9bfe05f482a4636d
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-08-01 01:05:04 -0600

    Feat: set up middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { fetchUserAttributes } from "@aws-amplify/auth";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  try {
+    const { email } = await fetchUserAttributes();
+    if (
+      !email ||
+      email.substring(email.indexOf("@") + 1, email.length) !== "ubcbiztech.com"
+    ) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } catch {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: "/admin/:path*",
+};
