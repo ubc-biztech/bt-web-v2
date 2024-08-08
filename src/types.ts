a519108bc31a4c0dbc5107b17cf048bce075e15f
m20arcusk <marcuskam20@gmail.com>
2024-08-08 15:09:09 -0700

    didn't mean to delete BasicInformation in types.ts so I added it back

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
+export type BasicInformation = {
+    fname: string
+    lname: string
+    major: string
+    gender: string
+    year: string
+    diet: string
+    heardFrom: string
+    faculty: string
+}
+
 export type RegQuestionAttributes = {
     label: {
         S: string;
