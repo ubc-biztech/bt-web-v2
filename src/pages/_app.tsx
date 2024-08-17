34adaa5f68d95c6fe2236eedeeb5929027866335
isa-leroux448 <isa.leroux448@gmail.com>
2024-08-16 20:34:14 -0700

    move head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import Layout from './layout';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "./layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
