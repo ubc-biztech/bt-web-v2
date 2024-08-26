34816ea2275f696f2080a5cf7724231a9b89ec9f
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-08-26 14:39:40 -0700

    Extract Component Refactoring + Tailwind Breakpoints

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,4 +1,6 @@
 import { EventDashboard } from "@/components/EventsDashboard/EventDashboard";
+import { FilterTab } from "@/components/EventsDashboard/FilterTab";
+import { SearchBar } from "@/components/EventsDashboard/SearchBar";
 import { fetchBackend } from "@/lib/db";
 import { BiztechEvent } from "@/types/types";
 import { getCurrentUser } from "@aws-amplify/auth";
@@ -21,7 +23,7 @@ const filterStates = {
 
 export default function Page({ events }: EventProps) {
   const [searchField, setSearchField] = useState("");
-  const [filterState, setFilterState] = useState("all");
+  const [filterState, setFilterState] = useState(filterStates.all);
   const isCooldownRef = useRef(false);
   // these useStates will be empty arrays by default, but currently have mocks before i verify backend integration works
   const [saved, setSaved] = useState<string[]>([]);
@@ -90,7 +92,7 @@ export default function Page({ events }: EventProps) {
 
   return (
     <main className="bg-primary-color h-screen w-full overflow-auto">
-      <div className="mx-auto pt-8 p-5 lg:pt-20 lg:l-20 lg:pr-20  flex flex-col">
+      <div className="mx-auto pt-8 p-5 lg:pl-20 lg:pr-20 flex flex-col">
         <span>
           <h2 className="text-white text-xl lg:text-[40px]">Event Dashboard</h2>
           <div className="flex items-center justify-between h-[40px]">
@@ -98,59 +100,13 @@ export default function Page({ events }: EventProps) {
           </div>
         </span>
         <div className="bg-navbar-tab-hover-bg h-[1px] my-4" />
-        <div className="flex flex-row space-x-3">
-          <div className="relative lg:mb-6 mb-3 w-full lg:w-[400px]">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3.5">
-              <SearchIcon width={15} height={15} color="#6578A8" />
-            </div>
-            <input
-              type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 text-dark-navy"
-              placeholder="Search for events by name"
-              value={searchField}
-              onChange={handleChange}
-            />
-          </div>
-          <div
-            className={`bg-events-card-bg p-2 h-[46px] rounded-lg flex-row justify-center items-center space-x-1 px-20 shrink hidden lg:flex cursor-pointer ${
-              filterState === filterStates.all ? "!bg-events-baby-blue" : ""
-            }`}
-            onClick={() => handleUiClick(filterStates.all)}
-          >
-            <ListIcon height={20} width={20} className={`${filterState === filterStates.all ? "text-events-user-card-bg fill-current" : ""}`} />{" "}
-            <p className={`${filterState === filterStates.all ? "text-events-user-card-bg" : ""} text-nowrap`}>All Events</p>
-          </div>
-          <div
-            className={`bg-events-card-bg p-2 h-[46px] rounded-lg flex-row justify-center items-center space-x-1 px-20 shrink hidden lg:flex cursor-pointer ${
-              filterState === filterStates.saved ? "!bg-events-baby-blue" : ""
-            }`}
-            onClick={() => handleUiClick(filterStates.saved)}
-          >
-            <Bookmark height={20} width={20} className={`${filterState === filterStates.saved ? "text-events-user-card-bg fill-current" : ""}`} />
-            <p className={`${filterState === filterStates.saved ? "text-events-user-card-bg" : ""}`}>Saved</p>
-          </div>
-        </div>
-        <div className="flex flex-row space-x-3 mb-6 lg:hidden">
-          <div
-            className={`bg-events-card-bg p-2 h-[46px] rounded-lg flex-row justify-center items-center space-x-1 shrink flex grow lg:hidden cursor-pointer ${
-              filterState === filterStates.all ? "!bg-events-baby-blue" : ""
-            }`}
-            onClick={() => handleUiClick(filterStates.all)}
-          >
-            <ListIcon height={20} width={20} className={`${filterState === filterStates.all ? "text-events-user-card-bg fill-current" : ""}`} />
-            <p className={`${filterState === filterStates.all ? "text-events-user-card-bg" : ""} text-nowrap`}>All Events</p>
-          </div>
-          <div
-            className={`bg-events-card-bg p-2 h-[46px] rounded-lg flex-row justify-center items-center space-x-1 shrink flex grow lg:hidden cursor-pointer ${
-              filterState === filterStates.saved ? "!bg-events-baby-blue" : ""
-            }`}
-            onClick={() => handleUiClick(filterStates.saved)}
-          >
-            <Bookmark height={20} width={20} className={`${filterState === filterStates.saved ? "text-events-user-card-bg fill-current" : ""}`} />
-            <p className={`${filterState === filterStates.saved ? "text-events-user-card-bg" : ""}`}>Saved</p>
-          </div>
+        <div className="flex flex-row gap-x-1.5 between lg:space-x-3 flex-wrap">
+          <SearchBar handleChange={handleChange} searchField={searchField} />
+          <FilterTab title="All Events" filter={filterStates.all} filterState={filterState} handleUiClick={handleUiClick} Icon={ListIcon} />
+          <FilterTab title="Saved" filter={filterStates.saved} filterState={filterState} handleUiClick={handleUiClick} Icon={Bookmark} />
         </div>
 
+        {displayedEvents.length === 0 ? <div className="text-[20px] text-white flex-row items-center">No events found...</div> : <></>}
         <EventDashboard events={displayedEvents} user={email} saved={saved} registered={registered} setSaved={setSaved} />
       </div>
     </main>
@@ -158,19 +114,27 @@ export default function Page({ events }: EventProps) {
 }
 
 export async function getStaticProps() {
-  let events = await fetchBackend({ endpoint: "/events", method: "GET", authenticatedCall: false });
+  try {
+    let events = await fetchBackend({ endpoint: "/events", method: "GET", authenticatedCall: false });
 
-  events.sort((a: BiztechEvent, b: BiztechEvent) => {
-    return new Date(b.startDate).getTime() - new Date(a.startDate).getTime();
-  });
+    events.sort((a: BiztechEvent, b: BiztechEvent) => {
+      return new Date(b.startDate).getTime() - new Date(a.startDate).getTime();
+    });
 
-  events = events.filter((e: BiztechEvent) => {
-    return e.isPublished;
-  });
+    events = events.filter((e: BiztechEvent) => {
+      return e.isPublished;
+    });
 
-  return {
-    props: {
-      events,
-    },
-  };
+    return {
+      props: {
+        events,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        events: [],
+      },
+    };
+  }
 }
