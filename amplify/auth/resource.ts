98c577b324f3528ac4a9774def357175f724e560
bennypc <52959626+bennypc@users.noreply.github.com>
2024-09-09 20:45:05 -0700

    auth shit

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,22 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
+    externalProviders: {
+      google: {
+        clientId: secret(
+          "505989219189-4thc1dtkilvom87p1pbf3imb44vbdr2h.apps.googleusercontent.com"
+        ),
+        clientSecret: secret("ToK8IH7biYuG6gpTmsNFw2ND"),
+        scopes: ["email"],
+        attributeMapping: {
+          email: "email"
+        }
+      },
+      callbackUrls: [
+        "http://localhost:3000/login",
+        "https://app.ubcbiztech.com/login"
+      ],
+      logoutUrls: ["http://localhost:3000/", "https://app.ubcbiztech.com/"]
+    }
+  }
 });
