53a4fd71cc932fa756e3e0253d3f174d0a8690f9
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-09-21 21:19:58 -0700

    Comment out save logic

--- src/components/EventsDashboard/EventCard.tsx
+++ src/components/EventsDashboard/EventCard.tsx
@@ -21,53 +21,53 @@ export const EventCard: React.FC<EventCardProps> = ({ event, user, registered, s
   const [fill, setFill] = useState(false);
   let dateString = new Date(event.startDate);
 
-  const handleSaveClick = async (e: React.MouseEvent<SVGSVGElement>, id: string, year: number) => {
-    e.preventDefault();
-    let isFavourite;
-    const eventId = `${id};${year}`;
-    let newSaved: string[] = saved;
-    if (saved.includes(eventId)) {
-      newSaved = saved.filter((s) => {
-        return s !== eventId;
-      });
-      isFavourite = false;
-      setFill(false);
-    } else {
-      newSaved.push(eventId);
-      isFavourite = true;
-      setFill(true);
-    }
-    setSaved(newSaved);
+  // const handleSaveClick = async (e: React.MouseEvent<SVGSVGElement>, id: string, year: number) => {
+  //   e.preventDefault();
+  //   let isFavourite;
+  //   const eventId = `${id};${year}`;
+  //   let newSaved: string[] = saved;
+  //   if (saved.includes(eventId)) {
+  //     newSaved = saved.filter((s) => {
+  //       return s !== eventId;
+  //     });
+  //     isFavourite = false;
+  //     setFill(false);
+  //   } else {
+  //     newSaved.push(eventId);
+  //     isFavourite = true;
+  //     setFill(true);
+  //   }
+  //   setSaved(newSaved);
 
-    if (!user) {
-      return;
-    }
+  //   if (!user) {
+  //     return;
+  //   }
 
-    const body = {
-      eventID: id,
-      year,
-      isFavourite,
-    };
+  //   const body = {
+  //     eventID: id,
+  //     year,
+  //     isFavourite,
+  //   };
 
-    try {
-      await fetchBackend({ endpoint: `/users/favEvent/${user}`, method: "PATCH", data: body });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  //   try {
+  //     await fetchBackend({ endpoint: `/users/favEvent/${user}`, method: "PATCH", data: body });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
 
   const timeStateIndicator = (ev: BiztechEvent) => {
     const startDate = new Date(ev.startDate);
     const deadline = new Date(ev.deadline);
     if (new Date() >= deadline && startDate >= new Date()) {
       return (
-        <div className="rounded-full font-poppin font-[700] px-3 py-1 text-white bg-secondary-color text-[8px] lg:text-[12px]  flex items-center">
+        <div className='rounded-full font-poppin font-[700] px-3 py-1 text-white bg-secondary-color text-[8px] lg:text-[12px]  flex items-center'>
           COMING UP
         </div>
       );
     } else if (startDate > new Date() && !registered.includes(`${ev.id};${ev.year}`)) {
       return (
-        <div className="rounded-full px-3 py-1 font-poppins font-[700]  text-white bg-events-coming-up text-[8px] lg:text-[12px] flex items-center">
+        <div className='rounded-full px-3 py-1 font-poppins font-[700]  text-white bg-events-coming-up text-[8px] lg:text-[12px] flex items-center'>
           REGISTER BY {`${deadline.getMonth() + 1}/${deadline.getDate()}`}
         </div>
       );
@@ -78,13 +78,18 @@ export const EventCard: React.FC<EventCardProps> = ({ event, user, registered, s
 
   const registeredIndicator = (ev: BiztechEvent) => {
     if (registered.includes(`${ev.id};${ev.year}`)) {
-      return <div className="rounded-full font-poppin font-[700] px-3 py-1 text-white bg-black text-[8px] lg:text-[12px]  flex items-center">REGISTERED</div>;
+      return (
+        <div className='rounded-full font-poppin font-[700] px-3 py-1 text-white bg-black text-[8px] lg:text-[12px]  flex items-center'>
+          REGISTERED
+        </div>
+      );
     } else {
       return <></>;
     }
   };
 
-  const dateText = `${months[dateString.getMonth()]} ${dateString.getDate()}, ${event.year}` + " " + `${dateString.toTimeString().slice(0, 5)}`;
+  const dateText =
+    `${months[dateString.getMonth()]} ${dateString.getDate()}, ${event.year}` + " " + `${dateString.toTimeString().slice(0, 5)}`;
 
   const eventPricingText =
     `${event.pricing && event.pricing > 0 ? "$" + event.pricing.members.toFixed(2) : "Free!"} ` +
@@ -102,22 +107,22 @@ export const EventCard: React.FC<EventCardProps> = ({ event, user, registered, s
           transition={{
             type: "tween",
             ease: "easeInOut",
-            duration: 0.3,
+            duration: 0.3
           }}
           onClick={(e) => e.stopPropagation()}
         >
-          <div className="bg-events-user-card-bg w-full p-3 rounded-[10px] my-2">
-            <div className="flex flex-row space-x-5 relative">
-              <div className="relative lg:w-[200px] lg:h-[130px] w-[100px] h-[75px] overflow-hidden rounded-lg shrink-0">
-                <Image className="object-cover" fill src={event.imageUrl} alt={event.ename} />
+          <div className='bg-events-user-card-bg w-full p-3 rounded-[10px] my-2'>
+            <div className='flex flex-row space-x-5 relative'>
+              <div className='relative lg:w-[200px] lg:h-[130px] w-[100px] h-[75px] overflow-hidden rounded-lg shrink-0'>
+                <Image className='object-cover' fill src={event.imageUrl} alt={event.ename} />
               </div>
-              <div className="flex flex-col space-y-1 grow">
-                <div className="font-600 text-sm lg:text-[24px] py-0.5 lg:py-2 flex flex-row space-x-3 items-center w-full">
+              <div className='flex flex-col space-y-1 grow'>
+                <div className='font-600 text-sm lg:text-[24px] py-0.5 lg:py-2 flex flex-row space-x-3 items-center w-full'>
                   <div>{event.ename}</div>
-                  <div className="grow"></div>
-                  <div className="hidden lg:block">{registeredIndicator(event)}</div>
-                  <div className="hidden lg:block">{timeStateIndicator(event)}</div>
-                  <div className="">
+                  <div className='grow'></div>
+                  <div className='hidden lg:block'>{registeredIndicator(event)}</div>
+                  <div className='hidden lg:block'>{timeStateIndicator(event)}</div>
+                  <div className=''>
                     {/* TODO: awaiting backend favEvent fix */}
                     {/* <Bookmark
                       height={30}
@@ -127,11 +132,11 @@ export const EventCard: React.FC<EventCardProps> = ({ event, user, registered, s
                     /> */}
                   </div>
                 </div>
-                <p className="text-[10px] lg:text-sm text-events-baby-blue">{dateText}</p>
-                <div className="flex flex-row items-center justify-between w-full">
-                  <p className="text-[10px] lg:text-sm text-events-baby-blue">{eventPricingText}</p>
-                  <div className="lg:hidden flex grow justify-end mr-0.5">{registeredIndicator(event)}</div>
-                  <div className="lg:hidden">{timeStateIndicator(event)}</div>
+                <p className='text-[10px] lg:text-sm text-events-baby-blue'>{dateText}</p>
+                <div className='flex flex-row items-center justify-between w-full'>
+                  <p className='text-[10px] lg:text-sm text-events-baby-blue'>{eventPricingText}</p>
+                  <div className='lg:hidden flex grow justify-end mr-0.5'>{registeredIndicator(event)}</div>
+                  <div className='lg:hidden'>{timeStateIndicator(event)}</div>
                 </div>
               </div>
             </div>
