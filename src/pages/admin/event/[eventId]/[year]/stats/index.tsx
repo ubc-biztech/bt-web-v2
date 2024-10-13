8adf8d613170676972d119e6ea4d9979519919da
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-10-13 11:22:45 -0700

    fix stats imports

--- src/pages/admin/event/[eventId]/[year]/stats/index.tsx
+++ src/pages/admin/event/[eventId]/[year]/stats/index.tsx
@@ -4,9 +4,9 @@ import { useRouter } from "next/router";
 import { fetchRegistrationData } from "@/lib/dbUtils";
 import PercentageBars from "@/components/Stats/PercentageBars";
 import ChartBox from "@/components/Stats/ChartBox";
+import StatsTable from "@/components/Stats/StatsTable";
 import BarChart from "@/components/Stats/BarChart";
 import PieChart from "@/components/Stats/PieChart";
-import StatsTable from "@/components/Stats/StatsTable";
 
 type Props = {
   initialData: AttendeeBasicInformation[] | null;
@@ -19,77 +19,55 @@ const getFieldCounts = (data: AttendeeBasicInformation[], field: string) => {
     const value = getNestedValue(userData, field);
 
     if (value !== undefined && value !== null) {
-      counts[String(value)] = (counts[String(value)] + 1 || 1);
+      counts[String(value)] = counts[String(value)] + 1 || 1;
     }
   }
-  return Object.entries(counts).sort().map(([label, value]) => ({ label, value }));
+  return Object.entries(counts)
+    .sort()
+    .map(([label, value]) => ({ label, value }));
 };
 
 const getNestedValue = (userData: Record<string, any>, field: string) => {
-    const keys = field.split(".");
-    for (const key of keys) {
-      userData = userData?.[key];
-    }
-    return userData;
-  };
+  const keys = field.split(".");
+  for (const key of keys) {
+    userData = userData?.[key];
+  }
+  return userData;
+};
 
 export default function Statistics({ initialData }: Props) {
   const router = useRouter();
 
   return (
-    <main className="bg-primary-color min-h-screen">
-      <div className="container mx-auto p-10 flex flex-col">
+    <main className='bg-primary-color min-h-screen'>
+      <div className='container mx-auto p-10 flex flex-col'>
         <span>
-          <h2 className="text-white">Event Statistics</h2>
-          <p className="text-baby-blue font-poppins">
+          <h2 className='text-white'>Event Statistics</h2>
+          <p className='text-baby-blue font-poppins'>
             Statistics {">"} {router.query.eventId} {router.query.year}
           </p>
           <br />
-          <ChartBox height="250px" title="Registration Status">
-            {initialData && (
-              <PercentageBars
-                data={getFieldCounts(initialData, "registrationStatus")}
-              />
-            )}
+          <ChartBox height='250px' title='Registration Status'>
+            {initialData && <PercentageBars data={getFieldCounts(initialData, "registrationStatus")} />}
           </ChartBox>
-          <div className="flex gap-0 flex-col lg:flex-row lg:gap-4">
-            <ChartBox width="33%" height="300px" title="Attendee Year Level">
-              {initialData && (
-                <BarChart
-                  data={getFieldCounts(initialData, "basicInformation.year")}
-                />
-              )}
+          <div className='flex gap-0 flex-col lg:flex-row lg:gap-4'>
+            <ChartBox width='33%' height='300px' title='Attendee Year Level'>
+              {initialData && <BarChart data={getFieldCounts(initialData, "basicInformation.year")} />}
             </ChartBox>
-            <ChartBox width="66%" height="300px" title="Heard Event From">
-              {initialData && (
-                <PieChart
-                  data={getFieldCounts(initialData, "basicInformation.heardFrom")}
-                />
-              )}
+            <ChartBox width='66%' height='300px' title='Heard Event From'>
+              {initialData && <PieChart data={getFieldCounts(initialData, "basicInformation.heardFrom")} />}
             </ChartBox>
           </div>
-          <div className="flex gap-0 flex-col lg:flex-row lg:gap-4">
-            <ChartBox width="66%" height="300px" title="Faculty">
-              {initialData && (
-                <PieChart
-                  data={getFieldCounts(initialData, "basicInformation.faculty")}
-                />
-              )}
+          <div className='flex gap-0 flex-col lg:flex-row lg:gap-4'>
+            <ChartBox width='66%' height='300px' title='Faculty'>
+              {initialData && <PieChart data={getFieldCounts(initialData, "basicInformation.faculty")} />}
             </ChartBox>
-            <ChartBox width="33%" height="300px" title="Dietary Restrictions">
-              {initialData && (
-                <StatsTable
-                  data={getFieldCounts(initialData, "basicInformation.diet")}
-                />
-              )}
+            <ChartBox width='33%' height='300px' title='Dietary Restrictions'>
+              {initialData && <StatsTable data={getFieldCounts(initialData, "basicInformation.diet")} />}
             </ChartBox>
           </div>
-          <ChartBox width="66%" height="300px" title="Pronouns">
-            {initialData && (
-              <PieChart
-                data={getFieldCounts(initialData, "basicInformation.gender")}
-              />
-            )}
+          <ChartBox width='66%' height='300px' title='Pronouns'>
+            {initialData && <PieChart data={getFieldCounts(initialData, "basicInformation.gender")} />}
           </ChartBox>
         </span>
       </div>
