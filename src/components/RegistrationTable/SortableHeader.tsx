3ae999d62aa681d33269ee3bc0feb24e0b8bd05b
liuisaac <80558145+liuisaac@users.noreply.github.com>
2024-11-11 03:29:10 -0800

    added extra hover effects to make feature obvious

--- src/components/RegistrationTable/SortableHeader.tsx
+++ src/components/RegistrationTable/SortableHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Column } from "@tanstack/react-table";
 import { CircleChevronDown, CircleChevronUp } from "lucide-react";
 
@@ -11,6 +11,7 @@ export const SortableHeader = <T,>({
     column,
     title,
 }: SortableHeaderProps<T>) => {
+    const [hover, setHover] = useState(false);
     return (
         <div
             className={`group cursor-pointer select-none ${
@@ -20,10 +21,25 @@ export const SortableHeader = <T,>({
                     : "hover:text-black text-white"
             } transition-colors duration-200 ease-in-out inline-flex gap-1`}
             onClick={column.getToggleSortingHandler()}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
         >
             {title}
-            {column.getIsSorted() === "asc" && <CircleChevronUp />}
-            {column.getIsSorted() === "desc" && <CircleChevronDown />}
+            <div
+                className={`${
+                    column.getIsSorted() === "asc"
+                        ? "text-biztech-green" // Color for ascending sort
+                        : column.getIsSorted() === "desc"
+                        ? "text-light-red" // Color for descending sort
+                        : "text-black" // Default color for unsorted
+                }`}
+            >
+                {(column.getIsSorted() === "asc" ||
+                    (hover && column.getIsSorted() != "desc")) && (
+                    <CircleChevronUp />
+                )}
+                {column.getIsSorted() === "desc" && <CircleChevronDown />}
+            </div>
         </div>
     );
 };
