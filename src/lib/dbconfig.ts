6cec4ee91db81fbaa3a05fb6522c08f4c39ee644
Victor <victorv@ubcbiztech.com>
2024-12-14 12:42:34 -0800

    Change .env flag for prod and change to v2 url

--- src/lib/dbconfig.ts
+++ src/lib/dbconfig.ts
@@ -4,15 +4,15 @@
 //     : aws_exports
 
 export const API_URL =
-process.env.NEXT_PUBLIC_REACT_APP_STAGE === "production"
+process.env.REACT_APP_STAGE === "production"
   ? "https://api.ubcbiztech.com"
-  : process.env.NEXT_PUBLIC_REACT_APP_STAGE === "local"
+  : process.env.REACT_APP_STAGE === "local"
     ? "http://localhost:4000"
     : "https://api-dev.ubcbiztech.com";
 
 export const CLIENT_URL =
-  process.env.NEXT_PUBLIC_REACT_APP_STAGE === "production"
-    ? "https://app.ubcbiztech.com/"
-    : process.env.NEXT_PUBLIC_REACT_APP_STAGE === "local"
+  process.env.REACT_APP_STAGE === "production"
+    ? "https://v2.ubcbiztech.com/"
+    : process.env.REACT_APP_STAGE === "local"
       ? "http://localhost:3000/"
-      : "https://dev.app.ubcbiztech.com/";
+      : "https://dev.v2.ubcbiztech.com/";
