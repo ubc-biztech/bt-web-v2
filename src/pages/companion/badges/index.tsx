a4dff7f62b460cf53c78ed8ef3d2d8c9ed174c08
isa-leroux448 <isa.leroux448@gmail.com>
2025-01-17 17:59:31 -0800

    add back badge page fetch

--- src/pages/companion/badges/index.tsx
+++ src/pages/companion/badges/index.tsx
@@ -8,6 +8,7 @@ import StartupExplorerIcon from "@/assets/2025/blueprint/badgeIcons/startup_expl
 import { StaticImageData } from "next/image";
 import { fetchBackend } from "@/lib/db";
 import { CompanionItemRow } from "@/components/ui/companion-item-row";
+import { COMPANION_PROFILE_ID_KEY } from "@/constants/companion";
 
 export interface Badge {
   questID: string;
@@ -34,6 +35,46 @@ const Badges = () => {
   const [badges, setBadges] = useState([]);
   const [completedBadges, setCompletedBadges] = useState(0);
   const [completedHiddenBadges, setCompletedHiddenBadges] = useState(0);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchBadges = async () => {
+      try {
+        const profileId = localStorage.getItem(COMPANION_PROFILE_ID_KEY);
+        if (!profileId) {
+          setError("Please log in to view your connections");
+          return;
+        }
+
+        const data = await fetchBackend({
+          endpoint: `/interactions/quests/${profileId}`,
+          method: "GET",
+          authenticatedCall: false,
+        });
+        const dataWithCompleteStatus = data.data.map(
+          (badge: Omit<Badge, "isComplete">) => ({
+            ...badge,
+            isComplete: badge.progress >= badge.threshold,
+          })
+        );
+        setBadges(dataWithCompleteStatus);
+        const completedCount = dataWithCompleteStatus.filter(
+          (badge: Badge) => badge.isComplete
+        ).length;
+        const completedHiddenCount = dataWithCompleteStatus.filter(
+          (badge: Badge) =>
+            badge.isComplete && hiddenBadges.includes(badge.questID)
+        ).length;
+        setCompletedBadges(completedCount);
+        setCompletedHiddenBadges(completedHiddenCount);
+      } catch (error) {
+        console.error("Error fetching badges:", error);
+        setError("Error fetching your badges");
+      }
+    };
+
+    fetchBadges();
+  }, []);
 
   return (
     <NavBarContainer>
@@ -57,40 +98,46 @@ const Badges = () => {
             selectedFilterOption={filter}
           />
         </div>
-        {badges &&
-          badges
-            .filter((badge: Badge) => {
-              const isHidden = hiddenBadges.includes(badge.questID);
-              if (filterOptions[filter] === "Hidden") {
-                return isHidden && badge.isComplete;
-              } else if (filterOptions[filter] === "Collected") {
-                return badge.isComplete;
-              } else if (filterOptions[filter] === "Incomplete") {
-                return !badge.isComplete && !isHidden;
-              } else {
-                return (!isHidden && !badge.isComplete) || badge.isComplete;
-              }
-            })
-            .map((badge: Badge, index: number) => (
-              <BadgeRow
-                key={index}
-                badge={badge}
-                isHidden={hiddenBadges.includes(badge.questID)}
-                badgeIcon={badgeIcons[badge.questID]}
-              />
-            ))}
-        {(filterOptions[filter] === "All" || filterOptions[filter] === "Incomplete") && 
-          <CompanionItemRow className="!before:bg-none border-dashed border-[rgba(206,234,255,0.4)]">
-            <div className="flex justify-center flex-col text-center">
-              <p className="font-medium text-white">
-                {`+${hiddenBadges.length - completedHiddenBadges} more hidden ${hiddenBadges.length - completedHiddenBadges === 1 ? "badge" : "badges"} to collect`}
-              </p>
-              <p className="text-[#808080] text-[12px] font-redhat">
-                Details will be revealed at the end of the event
-              </p>
-            </div>
-          </CompanionItemRow>
-        }
+        {error ? (
+          <div className="text-red-500 text-center mt-4">{error}</div>
+        ) : (
+          <>
+            {badges &&
+              badges
+                .filter((badge: Badge) => {
+                  const isHidden = hiddenBadges.includes(badge.questID);
+                  if (filterOptions[filter] === "Hidden") {
+                    return isHidden && badge.isComplete;
+                  } else if (filterOptions[filter] === "Collected") {
+                    return badge.isComplete;
+                  } else if (filterOptions[filter] === "Incomplete") {
+                    return !badge.isComplete && !isHidden;
+                  } else {
+                    return (!isHidden && !badge.isComplete) || badge.isComplete;
+                  }
+                })
+                .map((badge: Badge, index: number) => (
+                  <BadgeRow
+                    key={index}
+                    badge={badge}
+                    isHidden={hiddenBadges.includes(badge.questID)}
+                    badgeIcon={badgeIcons[badge.questID]}
+                  />
+                ))}
+            {(filterOptions[filter] === "All" ||filterOptions[filter] === "Incomplete") && (
+              <CompanionItemRow className="!before:bg-none border-dashed border-[rgba(206,234,255,0.4)]">
+                <div className="flex justify-center flex-col text-center">
+                  <p className="font-medium text-white">
+                    {`+${hiddenBadges.length - completedHiddenBadges} more hidden ${hiddenBadges.length - completedHiddenBadges === 1 ? "badge" : "badges"} to collect`}
+                  </p>
+                  <p className="text-[#808080] text-[12px] font-redhat">
+                    Details will be revealed at the end of the event
+                  </p>
+                </div>
+              </CompanionItemRow>
+            )}
+          </>
+        )}
       </div>
     </NavBarContainer>
   );
