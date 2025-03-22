86f3ea2325bcee7fe6e90538d6ba75fe545bfa83
alex-gour <alexgour06@gmail.com>
2025-03-21 23:54:34 -0700

    Fixed shadow / font ui

--- src/components/companion/productX/user/Dashboard.tsx
+++ src/components/companion/productX/user/Dashboard.tsx
@@ -81,8 +81,8 @@ const Dashboard: React.FC<DashboardProps> = ({ team_name, members, flat_records,
           {/* AVERAGE JUDGE SCORE */}
           <div className='flex flex-row h-44 gap-2'>
             <div className='w-1/3 h-full'>
-              <Box innerShadow={50} className='flex flex-col justify-center items-center'>
-                <header className='text-[#4CC8BD] text-[5em] -mt-5'>
+              <Box innerShadow={20} className='flex flex-col justify-center items-center'>
+                <header className='pb-2 text-[#4CC8BD] text-[3.5em] -mt-5'>
                   {calculateAverageJudgeScore(entries)}
                   /25
                 </header>
@@ -91,9 +91,9 @@ const Dashboard: React.FC<DashboardProps> = ({ team_name, members, flat_records,
             </div>
             {/* BEST AREA */}
             <div className='w-2/3 h-full'>
-              <Box innerShadow={50} className='flex flex-col justify-center items-start pl-16'>
+              <Box innerShadow={20} className='flex flex-col justify-center items-start pl-16'>
                 <div className='flex flex-col justify-center items-start'>
-                  <span className='text-lg -mb-2'>Best Area</span>
+                  <span className='text-md pb-2 -mb-2'>Best Area</span>
                   <header className='text-[#BC88FF] text-[2em]'>{bestArea}</header>
                 </div>
               </Box>
@@ -102,7 +102,7 @@ const Dashboard: React.FC<DashboardProps> = ({ team_name, members, flat_records,
           <div className='flex flex-row h-72 gap-2'>
             {/* BAR CHART */}
             <div className='w-1/2 h-full'>
-              <Box innerShadow={50} className='flex flex-col'>
+              <Box innerShadow={20} className='flex flex-col'>
                 <span className='text-md text-white mt-4 ml-8 -mb-8'>Scoring Distribution</span>
                 <BarChart data={transformFeedbackToBarChartData(entries)} height={300} />
               </Box>
@@ -110,7 +110,7 @@ const Dashboard: React.FC<DashboardProps> = ({ team_name, members, flat_records,
 
             {/* COMMENTS */}
             <div className='w-1/2 h-full'>
-              <Box innerShadow={50} className='p-4 pl-8 text-[#898BC3] flex flex-col gap-4'>
+              <Box innerShadow={20} className='p-4 pl-8 text-[#898BC3] flex flex-col gap-4'>
                 <span className='text-md text-white'>Comments</span>
                 {comments
                   .filter((comment) => comment.message.trim() !== "") // Remove empty messages
