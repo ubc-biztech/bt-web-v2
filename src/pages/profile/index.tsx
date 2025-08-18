eedb178f4a89040009c482995fce9ac7488be5ed
ETHAN <ethan.t.hansen@gmail.com>
2025-08-17 22:20:54 -0700

    fix: simplify profile for fast page loading

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -4,7 +4,6 @@ import { GetServerSideProps } from "next";
 import { runWithAmplifyServerContext } from "@/util/amplify-utils";
 import { Registration } from "@/types/types";
 import { BiztechEvent, User } from "@/types";
-import EventStack from "@/components/Blocks/EventStack";
 import HeaderCard from "@/components/ProfilePage/HeaderCard";
 import AttributesCard from "@/components/ProfilePage/AttributesCard";
 
@@ -13,20 +12,6 @@ interface ProfilePageProps {
   events: BiztechEvent[];
   error?: string;
 }
-
-const sortRegistrationsByDate = (registrations: Registration[]) => {
-  registrations.sort((a: Registration, b: Registration) => {
-    const hasCreatedAtA = "createdAt" in a && a.createdAt !== undefined;
-    const hasCreatedAtB = "createdAt" in b && b.createdAt !== undefined;
-
-    if (hasCreatedAtA && !hasCreatedAtB) return -1;
-    if (!hasCreatedAtA && hasCreatedAtA) return 1;
-    if (hasCreatedAtA && hasCreatedAtB)
-      return (b.createdAt as number) - (a.createdAt as number);
-    return b.updatedAt - a.updatedAt;
-  });
-  return registrations;
-};
 
 export default function ProfilePage({
   profileData,
@@ -55,9 +40,8 @@ export default function ProfilePage({
         lname={profileData.lname}
         userRole={userRole}
       />
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-full">
+      <div className="grid grid-cols-1 gap-4 w-full">
         <AttributesCard profileData={profileData} userRole={userRole} />
-        <EventStack events={[]} />
       </div>
     </div>
   );
@@ -65,8 +49,10 @@ export default function ProfilePage({
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   try {
+    const nextServerContext = { request: req, response: res };
+
     const userAttributes = await runWithAmplifyServerContext({
-      nextServerContext: { request: req, response: res },
+      nextServerContext,
       operation: (contextSpec) => fetchUserAttributes(contextSpec),
     });
 
@@ -75,11 +61,6 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
       throw new Error("No email found");
     }
 
-    const nextServerContext = {
-      request: req,
-      response: res,
-    };
-
     const profileData = await fetchBackendFromServer({
       endpoint: `/users/${email}`,
       method: "GET",
@@ -87,34 +68,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
       nextServerContext,
     });
 
-    const userRegistrations = await fetchBackend({
-      endpoint: `/registrations/?email=${email}`,
-      method: "GET",
-      authenticatedCall: false,
-    });
-
-    const sortedRegistrations: Registration[] = sortRegistrationsByDate(
-      userRegistrations.data,
-    ).slice(0, 3);
-
-    const events = await Promise.all(
-      sortedRegistrations.map(async (registration) => {
-        const eventID = registration["eventID;year"].split(";")[0];
-        const year = registration["eventID;year"].split(";")[1];
-        const event = await fetchBackend({
-          endpoint: `/events/?id=${eventID}&year=${year}`,
-          method: "GET",
-          authenticatedCall: false,
-        });
-
-        return event[event.length - 1];
-      }),
-    );
-
     return {
       props: {
         profileData,
-        events: events || [],
       },
     };
   } catch (error) {
@@ -122,7 +78,6 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     return {
       props: {
         profileData: null,
-        events: [],
         error:
           error instanceof Error
             ? error.message
