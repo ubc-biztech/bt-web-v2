6c31438f87ef08f8aa25cd4b542fabfcc3c3444c
liuisaac <80558145+liuisaac@users.noreply.github.com>
2025-08-19 23:39:28 -0700

    modified Login.tsx to use new status component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,10 +8,10 @@ import {
 } from "@aws-amplify/auth";
 import { fetchBackend, fetchBackendFromServer } from "@/lib/db";
 import Link from "next/link";
-import { CheckCircle, XCircle } from "lucide-react";
 import { GetServerSideProps } from "next";
 import { useRedirect } from "@/hooks/useRedirect";
 import PageLoadingState from "@/components/Common/PageLoadingState";
+import DiscordVerifyStatus from "./discord-verify-status";
 
 const LoginForm: React.FC = () => {
   // All the logic and UI from the current Login component goes here
@@ -412,54 +412,7 @@ const LoginForm: React.FC = () => {
               </div>
             </div>
           </div>
-        ) : statusPage === "success" ? (
-          <div className="bg-dark-slate px-6 py-12 shadow sm:rounded-lg sm:px-12 text-white">
-            <div className="px-6 py-12 rounded-2xl text-center max-w-md w-full">
-              <div className="flex justify-center mb-8 text-green-600">
-                <CheckCircle className="h-24 w-24 text-secondary-color" />
-              </div>
-              <h2 className="mt-6 text-center text-2xl font-[600] leading-9 tracking-tight text-white-blue mb-6">
-                Successfully linked accounts
-              </h2>
-              <p className="mt-2 text-white">
-                Your discord account was successfully linked to your membership.
-              </p>
-
-              <div className="mt-6">
-                <Link
-                  href="/"
-                  className="flex w-full justify-center rounded-md bg-biztech-green px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-dark-green focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  <p className="text-login-form-card">Go to Dashboard</p>
-                </Link>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="bg-dark-slate px-6 py-12 shadow sm:rounded-lg sm:px-12 text-white">
-            <div className="px-6 py-12 rounded-2xl text-center max-w-md w-full">
-              <div className="flex justify-center mb-8 text-green-600">
-                <XCircle className="h-24 w-24 text-[#FF4262]" />
-              </div>
-              <h2 className="mt-6 text-center text-2xl font-[600] leading-9 tracking-tight text-white-blue mb-6">
-                Failed to linked accounts
-              </h2>
-              <p className="mt-2 text-white">
-                We couldn&apos;t link your Discord account to your membership.
-                Please try again later or contact server admins.
-              </p>
-
-              <div className="mt-6">
-                <Link
-                  href="/"
-                  className="flex w-full justify-center rounded-md bg-[#FF4262] px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#df3a55] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  <p className="text-login-form-card">Go to Dashboard</p>
-                </Link>
-              </div>
-            </div>
-          </div>
-        )}
+        ) : <DiscordVerifyStatus status={statusPage === "success"} />}
       </div>
     </div>
   );
