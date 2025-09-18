ec5ea4faa2c36cb6a4fa15bb4df0ae4a8744fa50
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-09-17 23:13:01 -0700

    Remove paymentPrice from event registration payload

--- src/pages/event/[eventId]/[year]/register/index.tsx
+++ src/pages/event/[eventId]/[year]/register/index.tsx
@@ -299,10 +299,6 @@ export default function AttendeeFormRegister() {
             user?.isMember || samePricing() ? "" : "(Non-member)"
           }`,
           paymentImages: [event.imageUrl],
-          paymentPrice:
-            (user?.isMember
-              ? event.pricing?.members
-              : event.pricing?.nonMembers) * 100,
           paymentType: "Event",
           success_url: `${
             process.env.NEXT_PUBLIC_REACT_APP_STAGE === "local"
