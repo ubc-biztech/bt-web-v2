311556846214eb35972015e101b603b4baccf132
Elijah Zhao <elijahzhao24@gmail.com>
2025-10-01 22:35:56 -0600

    make columns for mobile

--- src/components/Events/EventPreview.tsx
+++ src/components/Events/EventPreview.tsx
@@ -43,7 +43,7 @@ export const EventPreview: React.FC<EventPreviewProps> = ({ form }) => {
             </h3>
 
             {/* Event Location and Date */}
-            <div className="flex flex-row items-center gap-4 w-full">
+            <div className="flex flex-col sm:flex-row items-center gap-2 w-full mt-2">
               <div className="rounded-md items-center px-2.5 py-1 font-[700] text-white bg-[#6578A8] text-[7px] md:text-[10px] lg:text-[12px] w-full flex whitespace-nowrap overflow-hidden">
                 <Building className="mr-1 w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 flex-shrink-0" />
                 <span className="truncate">
@@ -53,12 +53,7 @@ export const EventPreview: React.FC<EventPreviewProps> = ({ form }) => {
 
               <div className="rounded-md items-center px-2.5 py-1 font-[700] text-white bg-[#6578A8] text-[7px] md:text-[10px] lg:text-[12px] w-full flex whitespace-nowrap overflow-hidden">
                 <Calendar className="mr-1 w-2 h-2 sm:w-3 sm:h-3 md:w-4 md:h-4 flex-shrink-0" />
-                <span className="sm:hidden truncate">
-                  {form.watch("startDate")
-                    ? shortformatDate(form.watch("startDate").toISOString())
-                    : "Date"}
-                </span>
-                <span className="hidden sm:block truncate">
+                <span className="truncate">
                   {form.watch("startDate")
                     ? `${extractTime(form.watch("startDate").toISOString())} ${extractMonthDay(form.watch("startDate").toISOString())}`
                     : "Date"}
