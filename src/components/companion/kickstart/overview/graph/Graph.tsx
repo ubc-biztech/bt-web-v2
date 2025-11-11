78633591877b74e937ca4c24054d90bb78dabb43
liuisaac <80558145+liuisaac@users.noreply.github.com>
2025-11-10 19:57:51 -0800

    header and metric implementation

---
+++ src/components/companion/kickstart/overview/graph/Graph.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Graph = () => {
+  return (
+    <div className='w-3/5 h-full bg-[#B4B4B4]'>Graph</div>
+  )
+}
+
+export default Graph
--- src/components/companion/kickstart/overview/header/Header.tsx
+++ src/components/companion/kickstart/overview/header/Header.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { GlowButton } from '../../ui/GlowButton';
-import { useTeam } from '@/components/companion/events/Kickstart2025';
 import { Ticker } from '@/components/companion/kickstart/overview/header/Ticker';
 import { Plus } from 'lucide-react';
-const Header = ({}) => {
-    const { team } = useTeam();
-
+const Header = ({teamName} : {teamName: string}) => {
     return (
         <div className='w-full h-30 flex flex-col items-center jusitfy-center mt-14'>
             <div className="w-full flex flex-row items-end justify-between text-[24px]">
-                <header className='font-instrument text-[42px] flex items-end leading-none'>biz.ai</header>
+                <header className='font-instrument text-[42px] flex items-end leading-none'>{teamName}</header>
                 <Ticker />
                 <GlowButton href="/companion" height='h-10' width='w-48' icon={Plus}>
                     <span className='text-[14px]'>
