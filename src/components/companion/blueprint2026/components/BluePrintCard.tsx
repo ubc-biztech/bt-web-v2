b2765d7fe9e7d02ffaf0b1268aa1ce0968a6124f
liuisaac05 <liuisaac05@gmail.com>
2026-01-23 09:49:01 -0800

    blueprint ui visibility fixes

--- src/components/companion/blueprint2026/components/BluePrintCard.tsx
+++ src/components/companion/blueprint2026/components/BluePrintCard.tsx
@@ -12,7 +12,7 @@ export default function BluePrintCard({
       className={cn(
         className,
         "flex flex-col gap-1",
-        "rounded-sm bg-[#0A1428]/20 backdrop-blur-[2px] border-[0.3px] border-white/50 shadow-[inset_0_0_8px_rgba(255,255,255,0.3)] p-4",
+        "rounded-sm bg-[#0A1428]/60 backdrop-blur-[2px] border-[0.3px] border-white/50 shadow-[inset_0_0_32px_rgba(255,255,255,0.3)] p-4",
       )}
     >
       {children}
