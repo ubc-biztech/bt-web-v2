5754865c5b1063192da4ad95d1efd9d34f9be1e3
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2026-01-24 11:49:54 -0800

    hotfix open qr image

--- src/components/companion/blueprint2026/components/BluePrintCard.tsx
+++ src/components/companion/blueprint2026/components/BluePrintCard.tsx
@@ -11,7 +11,7 @@ export default function BluePrintCard({
     <div
       className={cn(
         className,
-        "flex flex-col gap-1",
+        "relative flex flex-col gap-1",
         "rounded-sm bg-[#0A1428]/60 backdrop-blur-[2px] border-[0.3px] border-white/50 shadow-[inset_0_0_32px_rgba(255,255,255,0.3)] p-4",
       )}
     >
